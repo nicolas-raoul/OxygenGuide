8302f45f9073d6a32e3e9790ed6efdc41a1eb814
Nicolas Raoul <nicolas.raoul@gmail.com>
2012-11-21 14:19:57 +0900

    Updated for Wikivoyage

--- generate_index.sh
+++ generate_index.sh
@@ -11,24 +11,24 @@ echo "<html>
 <body>
 <ul>" > ${output}
 
-for i in  Africa Antarctica Asia "South Asia" "Southeast Asia" Caribbean \
+for i in  "Africa" "Antarctica" "Asia" "South Asia" "Southeast Asia" "Caribbean" \
 "Central America" "Europe" "Middle East" "North America" "South America" \
-"Other destinations" "Travel topics" List_of_phrasebooks
+"Other destinations" "Travel topics" "List of phrasebooks"
 do
-echo -n "Looking for $i .. "
+echo -n "Looking for $i ... "
 c="<title>${i}</title>"
 link=`grep -lir "$c" articles`
 if test "$link"x != "x" 
 then
 echo  '<li><a href="'${link}'">'${i}'</a></li>' >> ${output}
-echo OK.
+echo OK
 else
-echo "not found."
+echo "Not found"
 fi
 done
 echo '</ul>
-<p>This content is based on work by all volunteers of <a href="http://wikitravel.org">Wikitravel</a>. Text is available
-+under <a href="http://creativecommons.org/licenses/by-sa/1.0/">Creative Commons Attribution-ShareAlike 1.0</a>. Comment
-+s welcome on <a href="http://wikitravel.org/en/User_talk:Nicolas1981">my Wikitravel user page</a>.</p>
+<p>This content is based on work by all volunteers of <a href="http://wikivoyage.org">Wikivoyage</a> and <a href="http://wikitravel.org">Wikitravel</a>.
+Text is available under <a href="http://creativecommons.org/licenses/by-sa/1.0/">Creative Commons Attribution-ShareAlike 1.0</a>.
+Comments welcome on <a href="https://en.wikivoyage.org/wiki/User:Nicolas1981">my user page</a>.</p>
 </body>
 </html>' >> ${output}

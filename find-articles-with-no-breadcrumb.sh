78bfb6a9d6c4f645ca245ae4a0efa40d0e2935cb
Nicolas Raoul <nicolas.raoul@gmail.com>
2012-12-05 16:08:38 +0900

    Removed obsolete script

--- find-articles-with-no-breadcrumb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-find ../oxygenpump/wikicode -type f \! -exec grep -qi -e "IsPartOf\|IsIn\|REDIRECT\|itinerary}}\|{{phrasebookguide}}\|disamb\|topic}}" {} \; -print \
-| grep -v "wikicode/.*%2F" \
-| sed -e "s/.*wikicode\//* http:\/\/wikitravel.org\/en\//g" -e "s/.wikicode//g" -e "s/%2F/\//g"

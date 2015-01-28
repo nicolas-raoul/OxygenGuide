4caba322452c7b3249ea08a5deb2c197906ea85b
Nicolas Raoul <nicolas.raoul@gmail.com>
2015-01-28 16:58:27 +0900

    Removed unneeded scripts

--- upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ZIPNAME=OxygenGuide_2013-12-16-a.zip
-echo "Uploading to Sourceforge"
-su - nicolas-raoul
-rsync -e ssh $ZIPNAME wvuploader,wikivoyage@frs.sourceforge.net:/home/frs/project/w/wi/wikivoyage/OxygenGuide/
